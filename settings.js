a7a581503cf24e420dd63eb00d0acdcfd5e95a0b
maliyabot1 <108556024+maliyabot1@users.noreply.github.com>
2022-07-07 17:58:04 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+940531617'] //Owner number in aive msg
+global.premium = ['+94760231617'] //Owner Number info
+global.ownernomer = '+94760531617' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
